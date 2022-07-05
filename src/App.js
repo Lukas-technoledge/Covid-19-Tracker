114671ed0d4c894ec330ef56a97595218415f493
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-07-05 05:50:17 -0700

    still working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Card, CardContent } from '@mui/material';
 import './App.css';
 import Header from './Header';
+import InfoBox from './InfoBox';
+import Map from './Map';
 
 function App() {
   return (
     <div className="app">
+    <div className="app__left">
       <Header />
       <div className="app__stats">
-        
+        <InfoBox title='Coronavirus Cases' cases={123} total={200}/>
+        <InfoBox title='Recovered' cases={123} total={200}/>
+        <InfoBox title='Deaths' cases={123} total={200}/>
       </div>
+      <Map />
+    </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/InfoBox.js
+++ src/InfoBox.js
@@ -4,8 +4,16 @@ import './InfoBox.css'
 
 function InfoBox({title, cases, total}) {
   return (
-    <Card>
-      
+    <Card className='infoBox'>
+    <CardContent>
+        <Typography className='infoBox__title' color= 'textSecondary'>
+            {title}
+        </Typography>
+        <h2 className='infoBox__cases'>{cases}</h2>
+        <Typography className='infoBox__total'>
+            {total} Total
+        </Typography>
+    </CardContent>
     </Card>
   )
 }
